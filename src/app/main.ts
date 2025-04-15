9876a6e85b01e376a2cdba4f3228a155972e19e8
Danila <devdaniellle@gmail.com>
2025-04-15 10:20:41 +0500

    fix dependencies

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,8 +1,8 @@
-import './assets/main.css'
+import '@/app/styles/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
+import router from '@/router/index'
 
 const app = createApp(App)
 
