2cf66889d9225ce30e66df055d4ca0baee69b71c
Danila <devdaniellle@gmail.com>
2025-05-15 14:14:54 +0500

    add deploy dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -6,13 +6,14 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  base:
+    process.env.NODE_ENV === 'production'
+      ? '/frontend/'
+      : '/',
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
