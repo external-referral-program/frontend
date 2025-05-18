6dee52810afd64558dbe15f5404d799384b1ffcf
Danila <devdaniellle@gmail.com>
2025-05-19 01:20:51 +0500

    fix endpoints

--- src/shared/config/api.ts
+++ src/shared/config/api.ts
@@ -1,8 +1,8 @@
 export const API_CONFIG = {
   BASE_URL: 'http://localhost:8000',
 
-  VACANCIES_ENDPOINT: '/api/vacanciesList/',
-  CITIES_ENDPOINT: '/api/citiesList/',
+  VACANCIES_ENDPOINT: '/api/vacancies-list/',
+  CITIES_ENDPOINT: '/api/cities-list/',
 
   SEND_REGISTRATION_CODE_ENDPOINT: 'api/referer/send-registration-code/',
   REGISTER_USER_ENDPOINT: 'api/referer/registration/',
