798085ca305e1e0bdcd461a15d8534fefe13220e
Danila <devdaniellle@gmail.com>
2025-05-19 14:21:25 +0500

    fix interceptors for public routes

--- src/shared/api/axiosSettings.ts
+++ src/shared/api/axiosSettings.ts
@@ -9,6 +9,12 @@ export const api = axios.create({
 })
 
 api.interceptors.request.use((config) => {
+  const publicRoutes = [API_CONFIG.VACANCIES_ENDPOINT, API_CONFIG.CITIES_ENDPOINT]
+
+  const isPublicRoute = publicRoutes.some((route) => config.url?.startsWith(route))
+
+  if (isPublicRoute) return config
+
   const token = localStorage.getItem('access_token')
   if (token) {
     config.headers.Authorization = `Token ${token}`
