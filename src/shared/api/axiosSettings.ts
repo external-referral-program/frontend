a11434c480fc325b0857f29eb70ea0c0863d1bbb
Danila <devdaniellle@gmail.com>
2025-05-20 15:18:37 +0500

    fix unauthorized on password reset and send code

--- src/shared/api/axiosSettings.ts
+++ src/shared/api/axiosSettings.ts
@@ -9,7 +9,12 @@ export const api = axios.create({
 })
 
 api.interceptors.request.use((config) => {
-  const publicRoutes = [API_CONFIG.VACANCIES_ENDPOINT, API_CONFIG.CITIES_ENDPOINT]
+  const publicRoutes = [
+    API_CONFIG.VACANCIES_ENDPOINT,
+    API_CONFIG.CITIES_ENDPOINT,
+    API_CONFIG.SEND_PASSWORD_RESET_CODE_ENDPOINT,
+    API_CONFIG.PASSWORD_RESET_ENDPOINT,
+  ]
 
   const isPublicRoute = publicRoutes.some((route) => config.url?.startsWith(route))
 
